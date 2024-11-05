184b222aa88e4bf43bab547486cec4b09bc70b94
norma13115 <normapadilla13115@gmail.com>
2024-11-04 20:35:11 -0800

    added index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+let weather = "rainy";
+if (weather === "rainy") {
+  console.log("Bring an umbrella");
+} else {
+  console.log("enjoy the sun!");
+}
+weather = "cloudy";
+if (weather === "rainy") {
+  console.log("Bring an umbrella");
+} else if (weather === "cloudy") {
+  console.log("Bring a jacket");
+} else {
+  console.log("enjoy the sun!");
+}
+let grade = 85;
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {                                                                                                             
+  console.log("C");
+} else if (grade >= 60) {                                                                                                      
+  console.log("D");
+} else {                                                                                                                
+  console.log("F");
+}       
